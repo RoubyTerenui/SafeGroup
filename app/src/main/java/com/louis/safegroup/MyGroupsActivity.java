729cc70af93ba01d13e1a0759992c8f93d7bef4c
Louis Blasselle <louis.blasselle@gmail.com>
2018-11-08 08:43:16 -0500

    Add MyGroupsActivity

---
+++ app/src/main/java/com/louis/safegroup/MyGroupsActivity.java
@@ -0,0 +1,29 @@
+package com.louis.safegroup;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+
+public class MyGroupsActivity extends AppCompatActivity {
+    ListView groups_listView;
+
+    ArrayList<String> myGroups;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_my_groups);
+
+        myGroups = new ArrayList<>();
+        myGroups.add("Famille");
+        myGroups.add("Amis");
+        myGroups.add("Sorties");
+
+        groups_listView = (ListView)findViewById(R.id.groups_listView);
+        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(this, R.layout.activity_my_groups, R.id.groups_listView, myGroups);
+        groups_listView.setAdapter(arrayAdapter);
+    }
+}
