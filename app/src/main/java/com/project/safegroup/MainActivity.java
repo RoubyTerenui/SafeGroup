1369bf668e5b5b4775144f057e2ecba2fc949180
Unknown <terenuirouby@live.fr>
2018-11-14 19:38:39 -0500

    Rectification Bug

--- app/src/main/java/com/project/safegroup/MainActivity.java
+++ app/src/main/java/com/project/safegroup/MainActivity.java
@@ -1,12 +1,14 @@
 package com.project.safegroup;
 
 import android.app.FragmentTransaction;
+import android.content.Intent;
 import android.content.res.Resources;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.constraint.ConstraintLayout;
 import android.support.design.widget.BottomNavigationView;
+import android.support.design.widget.Snackbar;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.view.ViewPager;
@@ -14,13 +16,21 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.firebase.ui.auth.AuthUI;
+import com.firebase.ui.auth.ErrorCodes;
+import com.firebase.ui.auth.IdpResponse;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.project.safegroup.NotificationRecap;
 import com.project.safegroup.ProblemQuad;
 import com.project.safegroup.R;
 import com.project.safegroup.SafeQuad;
 import com.project.safegroup.SectionStatePageAdapter;
 import com.project.safegroup.ThreeButtons;
+
+import java.util.Arrays;
 
 public class MainActivity extends AppCompatActivity {
     private ThreeButtons mainFragment;
@@ -34,6 +44,9 @@ public class MainActivity extends AppCompatActivity {
     private int localState;
     private int localStatePrecision;
     private int localGroup;
+    // 1 - Identifier for Sign-In Activity
+
+    private static final int RC_SIGN_IN = 123;
 
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
             = new BottomNavigationView.OnNavigationItemSelectedListener() {
@@ -56,12 +69,25 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
-        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
-        this.configureAndShowMainFragment();
+        checkLogin();
+
     }
 
+
+    public void checkLogin(){
+        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+
+        if (user != null) {
+            // User is signed in
+            setContentView(R.layout.activity_main);
+            BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
+            navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+            this.configureAndShowMainFragment();
+        } else {
+            // No user is signed in.
+            startSignInActivity();
+        }
+    }
     private void configureAndShowMainFragment(){
 
         mainFragment = (ThreeButtons) getSupportFragmentManager().findFragmentById(R.id.frame_layout_main);
@@ -133,4 +159,86 @@ public class MainActivity extends AppCompatActivity {
     public void chooseGroup(){
         //TODO -- Choisir les groupes à partir de la base de donnée
     }
+
+
+
+    // 2 - Launch Sign-In Activity
+
+    private void startSignInActivity(){
+
+        startActivityForResult(
+
+                AuthUI.getInstance()
+
+                        .createSignInIntentBuilder()
+
+                        .setTheme(R.style.LoginTheme)
+
+                        .setAvailableProviders(
+
+                                Arrays.asList(new AuthUI.IdpConfig.EmailBuilder().build(), new AuthUI.IdpConfig.GoogleBuilder().build()))
+
+                        .setIsSmartLockEnabled(false, true)
+
+                        .setLogo(R.drawable.family_icon)
+
+                        .build(),
+
+                RC_SIGN_IN);
+
+    }
+
+
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        // 4 - Handle SignIn Activity response on activity result
+        checkLogin();
+        this.handleResponseAfterSignIn(requestCode, resultCode, data);
+    }
+
+    //  - Show Snack Bar with a message
+    private void showToast( String message){
+        Toast.makeText(this, message, Snackbar.LENGTH_LONG).show();
+
+    }
+
+    // --------------------
+
+    // UTILS
+
+    // --------------------
+
+
+    // 3 - Method that handles response after SignIn Activity close
+
+    private void handleResponseAfterSignIn(int requestCode, int resultCode, Intent data){
+
+
+
+
+
+        if (requestCode == RC_SIGN_IN) {
+
+            IdpResponse response = IdpResponse.fromResultIntent(data);
+            if (resultCode == RESULT_OK) { // SUCCESS
+                showToast( "authentification success");
+            } else { // ERRORS
+
+                if (response == null) {
+                    showToast( "autentification failed");
+                } else if (response.getError().getErrorCode() == ErrorCodes.NO_NETWORK) {
+                    showToast("no Internet");
+                } else if (response.getError().getErrorCode() == ErrorCodes.UNKNOWN_ERROR) {
+                    showToast("Unknown error");;
+
+                }
+
+            }
+
+        }
+
+    }
+
 }
